f8232273184034498fb078c8fa3ce991680f5bcb
Thanh Huyen <teresathanhhuyen1804@gmail.com>
2017-05-09 16:00:34 +0700

    up

--- ThanhHuyen/modul1/1.php
+++ ThanhHuyen/modul1/1.php
@@ -1,9 +1,9 @@
 <html>
     <head>
         <title>Restaurant</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>    
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
