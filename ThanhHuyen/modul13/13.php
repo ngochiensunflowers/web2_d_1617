62405ed53d9579dc4373b7ad25d80bb0a33c342b
Thanh Huyen <teresathanhhuyen1804@gmail.com>
2017-05-09 16:08:50 +0700

    up

---
+++ ThanhHuyen/modul13/13.php
@@ -0,0 +1,97 @@
+<html>
+    <head>
+        <title>Restaurant</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+       
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/13.less', 'css/13.css');
+        ?>
+         <link href="css/13.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="modul-13">           
+                <div id="reviews">
+                    <div id="comments">
+                        <h1 class="title">3 reviews</h1>                       
+                        <ol class="commentlist">
+                            <li class="comment">
+                                <div class="comment-2">
+                                    <img src="images/David.jpeg" alt=""/>
+                                   <div class="comment-text">
+                                       <div class="star-rating">
+                                            <span style="width:100%"><strong>5</strong> out of 5</span>
+                                       </div>  
+                                       <p class="meta">
+                                            <strong class="woocommerce-review__author" itemprop="author">David</strong> 
+                                            <span class="woocommerce-review__dash">–</span>
+                                            <time class="woocommerce-review__published-date" itemprop="datePublished" datetime="2016-08-06T12:20:06+00:00">August 6, 2016</time>
+                                       </p>
+                                       <div class="description">
+                                           <p>Some review from user. Mmm tasty )</p>
+                                       </div>
+                                   </div>                  
+                                </div>                                                       
+                            </li>
+                            
+                            <li class="comment odd alt thread-odd thread-alt depth-1" id="li-comment-6">
+
+                                <div id="comment-6" class="comment_container">
+                                    <img src="images/David.jpeg" alt=""/>
+                                    <div class="comment-text">
+                                        <div class="star-rating">
+                                            <span style="width:80%"><strong>4</strong> out of 5</span>
+                                        </div>
+                                        
+                                        <p class="meta">
+                                            <strong class="woocommerce-review__author" itemprop="author">David</strong>
+                                            <span class="woocommerce-review__dash">–</span> 
+                                            <time class="woocommerce-review__published-date" itemprop="datePublished" datetime="2016-09-19T08:11:41+00:00">September 19, 2016</time>
+                                        </p>
+                                        
+                                        <div class="description">
+                                            <p>Lorem Ipsum</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            
+                            <li class="comment even thread-even depth-1" id="li-comment-18">
+
+                                <div id="comment-18" class="comment_container">
+                                    <img src="images/people.png" alt=""/>
+                                    <div class="comment-text">
+                                        <div class="star-rating">
+                                            <span style="width:80%">
+                                                <strong>4</strong>
+                                                out of 5
+                                            </span>
+                                        </div>
+
+                                        <p class="meta">
+                                            <strong class="woocommerce-review__author" itemprop="author">tester</strong> 
+                                            <em class="woocommerce-review__verified verified">(verified owner)</em> 
+                                            <span class="woocommerce-review__dash">–</span> 
+                                            <time class="woocommerce-review__published-date" itemprop="datePublished" datetime="2016-10-28T11:47:02+00:00">October 28, 2016</time>
+                                        </p>
+
+                                        <div class="description">
+                                            <p>yummy and delicious</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ol>
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
